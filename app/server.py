b2670503223c6809d8da55a5a9bf0121046fcb71
Jason Shi <xinshi93@gmail.com>
2019-01-17 23:09:04 -0800

    update

--- app/server.py
+++ app/server.py
@@ -11,7 +11,7 @@ from fastai.vision import *
 model_file_url = 'https://www.dropbox.com/s/l7qoi4yiuy871n3/model.pth?dl=1'
 model_file_name = 'model'
 
-classes = ['kim_kardashian','marie_kondo','trump','alexandria_ocasio-cortez', 'beyonce']
+classes = ['alexandria_ocasio-cortez','beyonce','kim_kardashian','marie_kondo','trump']
 
 path = Path(__file__).parent
 
