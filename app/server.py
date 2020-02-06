fddb90de4f9caa4ac43e2a83d5ce707b14aed67f
Jason Shi <xinshi93@gmail.com>
2020-02-05 21:35:27 -0800

    update server

--- app/server.py
+++ app/server.py
@@ -11,7 +11,8 @@ from fastai.vision import *
 model_file_url = 'https://www.dropbox.com/s/ogd2hm8aqu5q3va/model.pth?dl=1'
 model_file_name = 'model'
 
-classes = ['alexandria_ocasio-cortez','beyonce','brad_pitt','david_beckham','dwayne_johnson','kim_kardashian','kylie_jenner','marie_kondo','ryan_gosling','trump']
+# classes = ['alexandria_ocasio-cortez','beyonce','brad_pitt','david_beckham','dwayne_johnson','kim_kardashian','kylie_jenner','marie_kondo','ryan_gosling','trump']
+classes = ['alexandria ocasio cortez','beyonce','brad pitt','david beckham','dwayne johnson','kim kardashian','kylie jenner','marie kondo','ryan gosling','trump']
 
 path = Path(__file__).parent
 
